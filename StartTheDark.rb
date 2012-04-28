8b3f13773bde9a0133f05856f2e3cc9ab2bcc409
CristinaCaciur <caciur_cristina@yahoo.com>
2012-04-28 17:17:53 +0300

    formular de propunere

--- StartTheDark.rb
+++ StartTheDark.rb
@@ -2,20 +2,41 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/activerecord'
 
+enable :sessions
+
 class Activity < ActiveRecord::Base
+  has_many :participants
+  validates :description, :presence => true
 end
 
-class Participants < ActiveRecord::Base
+class Participant < ActiveRecord::Base
+  belongs_to :activity
+  validates :activity_id, :numericality => true
 end
 
 before do
   p "debug before filter"
   p request.ip
-  #Participants
+  p = Participant.find_by_ipaddress(request.ip)
+  if p.nil?
+    Participant.create(:ipaddress => request.ip, :nickname => request.ip, :activity_id => 0 )
+  end
 end
 
 get '/' do
   @activities = Activity.all
+  @allow_add_activity = false
+  a = Activity.find_by_author_ipaddress(request.ip)
+  if a.nil?
+    @allow_add_activity = true
+  end
   erb :index_d
 end
 
+post '/doaddactivity' do
+  a = Activity.create(:author_ipaddress => request.ip, :description => params[:activity][:description])
+  p = Participant.find_by_ipaddress(request.ip)
+  p.activity_id = a.id
+  p.save
+  redirect '/'
+end
