b44ad072e3c1a25a174b57f40d011163dbfc6a6e
CristinaCaciur <caciur_cristina@yahoo.com>
2012-04-28 15:01:16 +0300

    am adaugat HTML table

--- StartTheDark.rb
+++ StartTheDark.rb
@@ -8,7 +8,14 @@ end
 class Participants < ActiveRecord::Base
 end
 
+before do
+  p "debug before filter"
+  p request.ip
+  #Participants
+end
+
 get '/' do
+  @activities = Activity.all
   erb :index_d
 end
 
